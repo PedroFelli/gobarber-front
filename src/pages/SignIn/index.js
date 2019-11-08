79b90e905ceb47c664810e2bae788cc7b668197a
Pedro <pedrofellipeunai@gmail.com>
2019-11-07 22:53:58 -0300

    first commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form>
+        <Input type="email" placeholder="Seu email" />
+        <Input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
