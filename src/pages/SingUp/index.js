8041cbe6eb5c9448fdfa34862a495c74aa9faada
Pedro <pedrofellipeunai@gmail.com>
2019-11-08 23:57:41 -0300

    configuração da store/redux

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'Senha precisa de no minimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
